88a440533a028edfaad0ecf2288dee8172754c20
vinodchavan2209 <vinod.chavan2209@gmail.com>
2021-04-21 16:34:18 +0530

    first commit

---
+++ helloJava.java
@@ -0,0 +1,19 @@
+public class helloJava {
+    public static void main(String arg[]) {
+        System.out.println("Hello Java");
+        int o,m = 3;
+        int n = 6;
+
+        n = n + m; //n=9
+        System.out.println(n);
+        n += m; //n=9
+        System.out.println(n);
+        m = ++n; //pre increment  m = 7 and n = 7
+        System.out.println("m="+m);
+        System.out.println("n="+n);
+        System.out.println("************");
+        m = n++; //post increment m = 6 and n = 7
+        System.out.println("m="+m);
+        System.out.println("n="+n);
+    }
+}
